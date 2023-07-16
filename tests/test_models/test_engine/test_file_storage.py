fa12a9135f2f87addbb6dacca17ee38753c4a947
ManNjoro <elijohnmwoho@gmail.com>
2023-07-16 03:25:18 -0700

    added some edge cases in test_file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import unittest
 from time import sleep
 import json
+import os
 from os import remove
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
@@ -76,12 +77,56 @@ class test_fileStorage(unittest.TestCase):
         except FileNotFoundError:
             self.assertEqual(1, 2)
 
-    def test_pep8_conformance_file_storage(self):
+    def test_pycodestyle_file_storage(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/engine/file_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+    def test_file_storage_reload(self):
+        """Test the reload method of FileStorage"""
+        storage = FileStorage()
+
+        storage.save()
+
+        # Get the path to objects.json
+        root = os.path.dirname(os.path.abspath("console.py"))
+        path = os.path.join(root, "file.json")
+
+        # Read the lines before removing the file
+        with open(path, 'r') as file:
+            lines = file.readlines()
+
+        try:
+            os.remove(path)
+        except Exception:
+            pass
+
+        storage.save()
+
+        # Read the lines after saving the file
+        with open(path, 'r') as file:
+            lines2 = file.readlines()
+
+        self.assertEqual(lines, lines2)
+
+        try:
+            os.remove(path)
+        except Exception:
+            pass
+
+        # Create an empty objects.json file
+        with open(path, "w") as file:
+            file.write("{}")
+
+        # Verify the contents of the file
+        with open(path, "r") as file:
+            for line in file:
+                self.assertEqual(line, "{}")
+
+        # Test the reload method
+        self.assertIs(storage.reload(), None)
 
 
 if __name__ == '__main__':
